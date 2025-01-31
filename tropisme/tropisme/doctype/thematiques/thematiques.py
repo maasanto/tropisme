1f17ab84ef85d653081891b8d5fb62b34266ae46
Antoine Maas <antoine.maas@gmail.com>
2025-01-31 14:57:49 +0100

    refactor: removed doctype thematique-not included for simplification

--- tropisme/tropisme/doctype/thematiques/thematiques.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2024, Dokos SAS and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class Thematiques(Document):
-	pass
